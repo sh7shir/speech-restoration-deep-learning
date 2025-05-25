d3c830a7e732923ad627820578d1393553bcb953
Mahidul Alam Shishir <64400611+sh7shir@users.noreply.github.com>
2025-05-25 05:47:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
+from src.feature_extractor import extract_features_for_all_participants
+from src.audio_constructor import AudioReconstructor
+import src.config as config
+from src.vis import plot_correlation, plot_stgis, plot_history, plot_spectrograms
+import pdb
+import warnings
+
+warnings.filterwarnings("ignore")
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io import loadmat  #EEG dataset loading
+
+if config.extract_features:
+    extract_features_for_all_participants()
+
+if config.construct:
+    reconstruct = AudioReconstructor()
+    reconstruct.reconstruct()
+
+if config.visualization:
+    plot_correlation()
+    plot_stgis()
+    plot_history()
+    plot_spectrograms()
